bbbe2ec5c5f408f41fe924a4435b35eaca748f9d
sam <SamParklcf@hotmail.com>
2024-12-02 22:42:52 +0330

    publish task 3

--- lib1/build.gradle.kts
+++ lib1/build.gradle.kts
@@ -77,7 +77,7 @@ publishing {
     repositories {
         maven {
             name = "GitHubPackages"
-            url = uri("https://maven.pkg.github.com/octocat/hello-world")
+            //url = uri("https://maven.pkg.github.com/octocat/hello-world")
             credentials {
                 username = System.getenv("GITHUB_ACTOR")
                 password = System.getenv("GITHUB_TOKEN")
